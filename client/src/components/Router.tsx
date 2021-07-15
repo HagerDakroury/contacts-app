4da570a9569896583415f75abb127a6b1fd9f6bc
Hager Eldakroury <hagerdakroury@gmail.com>
2021-07-15 18:34:47 +0200

    feat: Add conditional rendering

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -1,24 +1,42 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { Landing } from "../pages/landing/Landing";
 import { Signup } from "../pages/signup/Signup";
 import { Signin } from "../pages/signin/Signin";
 import { ContactsList } from "../pages/contacts/ContactsList";
 
 export const MainRouter = () => {
+  if (!localStorage.getItem("user"))
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/signin">
+            <Signin />
+          </Route>
+          <Route path="/">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    );
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <Landing />
-        </Route>
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-        <Route exact path="/signin">
-          <Signin />
-        </Route>
         <Route exact path="/dashboard">
           <ContactsList />
+        </Route>
+        <Route path="/">
+          <Redirect to="/dashboard" />
         </Route>
       </Switch>
     </Router>
